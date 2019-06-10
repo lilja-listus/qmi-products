869de2218a986d91d845ef5264d4b2563c230acd
Lilja Listus <lilja.listus@gmail.com>
2019-06-10 19:35:28 +0300

    Add menu.

--- app/src/Routes.js
+++ app/src/Routes.js
@@ -2,11 +2,15 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom"; //will make the props awailable for all the nested components.
 import Signin from "./user/Signin";
 import Signup from "./user/Signup";
+import Home from "./core/Home";
+import Menu from "./core/Menu";
 
 const Routes = () => {
   return (
     <BrowserRouter>
+      <Menu />
       <Switch>
+        <Route path="/" exact component={Home} />
         <Route path="/signin" exact component={Signin} />
         <Route path="/signup" exact component={Signup} />
       </Switch>
