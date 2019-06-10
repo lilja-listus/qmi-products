1f36bac320b72d237a46633c8fea73905b61d952
Lilja Listus <lilja.listus@gmail.com>
2019-06-10 19:52:35 +0300

    Add config.

--- app/src/user/Signup.js
+++ app/src/user/Signup.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Layout from "../core/Layout";
+import { API } from "../config";
 
 const Signup = () => (
   <Layout title="Signup" description="Sign up to QMI web shop">
-    {process.env.REACT_APP_API_URL}
+    {API}
   </Layout>
 );
 
