2a75497277225b970baba3093a24cc7b044890e9
Lilja Listus <lilja.listus@gmail.com>
2019-06-11 14:24:10 +0300

    Create signup functionality.

--- app/src/user/Signup.js
+++ app/src/user/Signup.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import Layout from "../core/Layout";
+import { signup } from "../auth";
 
 const Signup = () => {
   const [values, setValues] = useState({
@@ -14,6 +16,27 @@ const Signup = () => {
     setValues({ ...values, error: false, [name]: event.target.value });
   };
 
+  const { name, email, password, success, error } = values;
+
+  const clickSubmit = event => {
+    event.preventDefault(); //so that browser is not reloaded when the button is clicked.
+    setValues({ ...values, error: false });
+    signup({ name, email, password }).then(data => {
+      if (data.error) {
+        setValues({ ...values, error: data.error, success: false });
+      } else {
+        setValues({
+          ...values,
+          name: "",
+          email: "",
+          password: "",
+          error: "",
+          success: true
+        });
+      }
+    });
+  };
+
   const singUpForm = () => (
     <form>
       <div className="form-group">
@@ -22,6 +45,7 @@ const Signup = () => {
           onChange={handleChange("name")}
           type="text"
           className="form-control"
+          value={name}
         />
       </div>
 
@@ -31,6 +55,7 @@ const Signup = () => {
           onChange={handleChange("email")}
           type="email"
           className="form-control"
+          value={email}
         />
       </div>
 
@@ -40,10 +65,31 @@ const Signup = () => {
           onChange={handleChange("password")}
           type="password"
           className="form-control"
+          value={password}
         />
       </div>
-      <button className="btn btn-primary">Submit</button>
+      <button onClick={clickSubmit} className="btn btn-primary">
+        Submit
+      </button>
     </form>
+  );
+
+  const showError = () => (
+    <div
+      className="alert alert-danger"
+      style={{ display: error ? "" : "none" }}
+    >
+      {error}
+    </div>
+  );
+
+  const showSuccess = () => (
+    <div
+      className="alert alert-info"
+      style={{ display: success ? "" : "none" }}
+    >
+      New account is created. Please <Link to="/signin">Signin</Link>
+    </div>
   );
 
   return (
@@ -52,6 +98,8 @@ const Signup = () => {
       description="Sign up to QMI web shop"
       className="container col-md-8 offset-md-2"
     >
+      {showSuccess()}
+      {showError()}
       {singUpForm()}
     </Layout>
   );
