9fdaa4910e27131067e45e3a39e1c41d07b5de42
Lilja Listus <lilja.listus@gmail.com>
2019-06-14 17:19:23 +0300

    Radio buttons for price range.

---
+++ app/src/core/RadioBox.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect, Fragment } from "react";
+
+const RadioBox = ({ prices }) => {
+  const [value, setValue] = useState(0);
+
+  const handleChange = () => {
+    //
+  };
+
+  return prices.map((
+    p,
+    i //key={i}  - so that each of them have unique index
+  ) => (
+    <div key={i}>
+      <input
+        onChange={handleChange}
+        value={`${p._id}`}
+        type="radio"
+        className="mr-2 ml-4"
+      />
+      <label className="form-check-label">{p.name}</label>
+    </div>
+  ));
+};
+
+export default RadioBox;
