d46ca502e4fff523720466812b81c684de22a610
Lilja Listus <lilja.listus@gmail.com>
2019-07-01 18:20:04 +0300

    Create methods to update user.

--- app/src/user/UserDashboard.js
+++ app/src/user/UserDashboard.js
@@ -21,7 +21,7 @@ const Dashboard = () => {
             </Link>
           </li>
           <li class="list-group-item">
-            <Link className="nav-link" to="/profile/update">
+            <Link className="nav-link" to={`/profile/${_id}`}>
               Update Profile
             </Link>
           </li>
