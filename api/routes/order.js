980056dff8ad18edef872bb6387a081ece94f60e
Lilja Listus <lilja.listus@gmail.com>
2019-07-01 14:34:35 +0300

    Add status to show.

--- api/routes/order.js
+++ api/routes/order.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById, addOrderToUserHistory } = require("../controllers/user");
-const { create, listOrders } = require("../controllers/order");
+const { create, listOrders, getStatusValues } = require("../controllers/order");
 const { decreaseQuantity } = require("../controllers/product");
 
 router.post(
@@ -15,6 +15,14 @@ router.post(
 );
 
 router.get("/order/list/:userId", requireSignin, isAuth, isAdmin, listOrders);
+
+router.get(
+  "/order/status-values/:userId",
+  requireSignin,
+  isAuth,
+  isAdmin,
+  getStatusValues
+);
 
 router.param("userId", userById);
 
