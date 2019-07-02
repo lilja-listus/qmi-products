79c644752715279069a882a94e79c2f8ba20f04c
Lilja Listus <lilja.listus@gmail.com>
2019-07-02 17:17:43 +0300

    User purchase history.

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { userById, read, update } = require("../controllers/user");
+const {
+  userById,
+  read,
+  update,
+  purchaseHistory
+} = require("../controllers/user");
 
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 
@@ -13,6 +18,7 @@ router.get("/secret/:userId", requireSignin, isAuth, isAdmin, (req, res) => {
 
 router.get("/user/:userId", requireSignin, isAuth, read);
 router.put("/user/:userId", requireSignin, isAuth, update);
+router.get("/orders/by/user/:userId", requireSignin, isAuth, purchaseHistory);
 
 router.param("userId", userById);
 
