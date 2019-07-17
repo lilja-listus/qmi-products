580b6fc7f887eb7479c2055d3f18f7bf2cd0a225
Lilja Listus <lilja.listus@gmail.com>
2019-07-17 15:13:51 +0300

    Reverse changes in add and update Product.

--- app/src/admin/UpdateProduct.jsx
+++ app/src/admin/UpdateProduct.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Layout from "../core/Layout";
 import { isAuthenticated } from "../auth";
-import { Link, Redirect } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 import { getProduct, getCategories, updateProduct } from "./apiAdmin";
 
 const UpdateProduct = ({ match }) => {
@@ -22,6 +22,7 @@ const UpdateProduct = ({ match }) => {
   });
 
   const { user, token } = isAuthenticated();
+
   const {
     name,
     description,
@@ -42,7 +43,6 @@ const UpdateProduct = ({ match }) => {
       if (data.error) {
         setValues({ ...values, error: data.error });
       } else {
-        // populate the state
         setValues({
           ...values,
           name: data.name,
@@ -53,22 +53,17 @@ const UpdateProduct = ({ match }) => {
           quantity: data.quantity,
           formData: new FormData()
         });
-        // load categories
         initCategories();
       }
     });
   };
 
-  // load categories and set form data
   const initCategories = () => {
     getCategories().then(data => {
       if (data.error) {
         setValues({ ...values, error: data.error });
       } else {
-        setValues({
-          categories: data,
-          formData: new FormData()
-        });
+        setValues({ categories: data, formData: new FormData() });
       }
     });
   };
@@ -102,7 +97,7 @@ const UpdateProduct = ({ match }) => {
             loading: false,
             error: false,
             redirectToProfile: true,
-            createdProduct: data.name
+            updatedProduct: data.name
           });
         }
       }
@@ -166,15 +161,6 @@ const UpdateProduct = ({ match }) => {
       </div>
 
       <div className="form-group">
-        <label className="text-muted">Shipping</label>
-        <select onChange={handleChange("shipping")} className="form-control">
-          <option>Please select</option>
-          <option value="0">No</option>
-          <option value="1">Yes</option>
-        </select>
-      </div>
-
-      <div className="form-group">
         <label className="text-muted">Quantity</label>
         <input
           onChange={handleChange("quantity")}
@@ -184,6 +170,14 @@ const UpdateProduct = ({ match }) => {
         />
       </div>
 
+      <div className="form-group">
+        <label className="text-muted">Shipping</label>
+        <select onChange={handleChange("shipping")} className="form-control">
+          <option>Please select</option>
+          <option value="0">No</option>
+          <option value="1">Yes</option>
+        </select>
+      </div>
       <button className="btn btn-outline-primary">Update Product</button>
     </form>
   );
@@ -202,16 +196,9 @@ const UpdateProduct = ({ match }) => {
       className="alert alert-info"
       style={{ display: createdProduct ? "" : "none" }}
     >
-      <h2>{`${createdProduct}`} is updated!</h2>
+      <h2>{`${createdProduct}`} is updated</h2>
     </div>
   );
-
-  const showLoading = () =>
-    loading && (
-      <div className="alert alert-success">
-        <h2>Loading...</h2>
-      </div>
-    );
 
   const redirectUser = () => {
     if (redirectToProfile) {
@@ -220,6 +207,13 @@ const UpdateProduct = ({ match }) => {
       }
     }
   };
+
+  const showLoading = () =>
+    loading && (
+      <div className="alert alert-success">
+        <h2>Loading ... </h2>
+      </div>
+    );
 
   return (
     <Layout
