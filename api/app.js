a5b26fc955706089b56e54a3c21d3307525fb9b0
Lilja Listus <lilja.listus@gmail.com>
2019-07-17 13:41:30 +0300

    change mongo connection.

--- api/app.js
+++ api/app.js
@@ -21,10 +21,7 @@ const app = express();
 
 //db
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true
-  })
+  .connect("mongod://127.0.0.1:27017/qmi", { useNewUrlParser: true })
   .then(() => {
     console.log("DB Connected");
   })
