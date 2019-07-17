e1d68582acac48ca14afb989ce2f1b904aec198a
Lilja Listus <lilja.listus@gmail.com>
2019-07-17 13:33:10 +0300

    Update model.

--- api/models/product.js
+++ api/models/product.js
@@ -6,19 +6,16 @@ const productSchema = new mongoose.Schema(
     name: {
       type: String,
       trim: true,
-      required: true,
-      maxLength: 32
+      required: true
     },
     description: {
       type: String,
-      required: true,
-      maxLength: 2000
+      required: true
     },
     price: {
       type: Number,
       trim: true,
-      required: true,
-      maxLength: 32
+      required: true
     },
     category: {
       type: ObjectId,
@@ -38,6 +35,7 @@ const productSchema = new mongoose.Schema(
       data: Buffer,
       contentType: String
     },
+
     shipping: {
       required: false,
       type: Boolean
